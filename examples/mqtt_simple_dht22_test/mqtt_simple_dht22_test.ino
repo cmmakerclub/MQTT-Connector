911e7e096a3b9b4989dca186663b613840f985c5
Nat Weerawan <nat.wrw@gmail.com>
2015-06-23 14:32:51 +0700

    Update example.

--- examples/mqtt_simple_dht22_test/mqtt_simple_dht22_test.ino
+++ examples/mqtt_simple_dht22_test/mqtt_simple_dht22_test.ino
@@ -1,4 +1,3 @@
-
 #define DEBUG_MODE
 
 #include <ESP8266WiFi.h>
@@ -21,17 +20,8 @@ const char* pass = "5k,skrijv',7'sik";
 // const char* pass = "gfkgvkgv'2015!!!!";
 
 
-
-#define WIFI_MAX_RETRIES 1500
-#define WIFI_CONNECT_DELAY_MS 20
-
-
-
 MqttWrapper *mqtt;
 DHT *dht;
-
-
-
 
 void connect_wifi()
 {
@@ -72,15 +62,14 @@ void callback(const MQTT::Publish& pub) {
     }
 }
 
-void hook_before_publish(JsonObject** root) {
+void hook_prepare_data(JsonObject** root) {
   JsonObject& data = (*(*root))["d"];
 
   static float t_dht;
-  static float h_dht; 
-
+  static float h_dht;
   read_dht(dht, &t_dht, &h_dht);
+  data["myName"] = "SIMPLE-DHT22-TEST";
 
-  data["myName"] = "NAT";
   data["temp"] = t_dht;
   data["humid"] = h_dht;
 }
@@ -89,25 +78,29 @@ void setup() {
     Serial.begin(115200);
     pinMode(0, INPUT_PULLUP);
     delay(10);
+    Serial.println();
+    Serial.println();
 
     connect_wifi();
     init_dht(&dht, DHTPIN, DHTTYPE);
 
     mqtt = new MqttWrapper("128.199.104.122");
     mqtt->connect(callback);
-    mqtt->set_prepare_publish_data_hook(hook_before_publish, 5000);
+    mqtt->set_prepare_data_hook(hook_prepare_data);
 }
 
 void loop() {
     reconnect_wifi_if_link_down();
     mqtt->loop();
 
+    // ตรวจจับการกด Switch
     if (digitalRead(0) == LOW) {
+        // วนลูปจนกว่าจะเอาปล่อย Switch
         while(digitalRead(0) == LOW) { 
             mqtt->loop();
             yield(); 
         }
-        mqtt->sync_pub(String("0"));
+        String status = "0";
+        mqtt->sync_pub(status);
     }
-
 }
--- examples/mqtt_simple_test/mqtt_simple_test.ino
+++ examples/mqtt_simple_test/mqtt_simple_test.ino
@@ -1,4 +1,3 @@
-
 #define DEBUG_MODE
 
 #include <ESP8266WiFi.h>
@@ -9,21 +8,17 @@
 // const char* ssid     = "OpenWrt_NAT_500GP.101";
 // const char* pass = "activegateway";
 
-const char* ssid     = "MAKERCLUB-CM";
-const char* pass = "welcomegogogo";
+// const char* ssid     = "MAKERCLUB-CM";
+// const char* pass = "welcomegogogo";
 
-// const char* ssid     = "Opendream Play";
-// const char* pass = "5k,skrijv',7'sik";
+const char* ssid     = "Opendream Play";
+const char* pass = "5k,skrijv',7'sik";
 
 // const char* ssid     = "Opendream";
 // const char* pass = "gfkgvkgv'2015!!!!";
 
-#define WIFI_MAX_RETRIES 1500
-#define WIFI_CONNECT_DELAY_MS 20
 
 MqttWrapper *mqtt;
-
-
 
 void connect_wifi()
 {
@@ -42,10 +37,11 @@ void connect_wifi()
 
 void reconnect_wifi_if_link_down() {
     if (WiFi.status() != WL_CONNECTED) {
-        Serial.println("WIFI DISCONNECTED");
+        DEBUG_PRINTLN("WIFI DISCONNECTED");
         connect_wifi();
     }
 }
+
 
 void callback(const MQTT::Publish& pub) {
     if (pub.payload_string() == "0") {
@@ -63,36 +59,40 @@ void callback(const MQTT::Publish& pub) {
     }
 }
 
-void hook_before_publish(JsonObject** root) {
+void hook_prepare_data(JsonObject** root) {
   JsonObject& data = (*(*root))["d"];
 
-  data["myName"] = "NAT";
+  data["myName"] = "SIMPLE-TEST";
+  data["adc"] = analogRead(A0);;
+
 }
 
 void setup() {
     Serial.begin(115200);
     pinMode(0, INPUT_PULLUP);
     delay(10);
+    Serial.println();
+    Serial.println();
 
     connect_wifi();
 
     mqtt = new MqttWrapper("128.199.104.122");
     mqtt->connect(callback);
-    mqtt->set_prepare_publish_data_hook(hook_before_publish, 5000);
+    mqtt->set_prepare_data_hook(hook_prepare_data);
 }
-
-
 
 void loop() {
     reconnect_wifi_if_link_down();
     mqtt->loop();
 
+    // ตรวจจับการกด Switch
     if (digitalRead(0) == LOW) {
+        // วนลูปจนกว่าจะเอาปล่อย Switch
         while(digitalRead(0) == LOW) { 
             mqtt->loop();
             yield(); 
         }
-        mqtt->sync_pub(String("0"));
+        String status = "0";
+        mqtt->sync_pub(status);
     }
-
 }
