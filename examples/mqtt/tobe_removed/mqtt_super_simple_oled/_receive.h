f060b0576807445fbfa6ad5134ce91cef56ae4ad
Nat Weerawan <nat.wrw@gmail.com>
2015-10-09 22:53:40 +0700

    Remove all examples.

--- examples/mqtt/tobe_removed/mqtt_super_simple_oled/_receive.h
+++
@@ -1,8 +0,0 @@
-PubSubClient::callback_t on_message_arrived = 
-[&](const MQTT::Publish & pub) -> void {
-    Serial.print("ON MESSAGE: ");
-    Serial.print(pub.topic());
-    Serial.print(" => ");
-    Serial.println(pub.payload_string());
-    celsius = pub.payload_string();
- };
--- examples/mqtt/tobe_removed/mqtt_super_switch/_publish.h
+++
@@ -1,7 +0,0 @@
-MqttConnector::prepare_data_hook_t on_prepare_data = 
-[&](JsonObject * root) -> void {
-    JsonObject& data = root->at("d");
-    data["myName"] = "TONG";
-    data["tag"] = "cmmc";
-    data["zone"] = "1";
-};
