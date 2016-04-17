57248467ef21b62fd6f59fddff0f17860bd5ffec
Nat Weerawan <nat.wrw@gmail.com>
2016-04-17 14:15:49 +0700

    Add platformio structure support.

---
+++ examples/basic_mqtt/_publish.h
@@ -0,0 +1,16 @@
+MqttConnector::prepare_data_hook_t on_prepare_data =
+[&](JsonObject * root) -> void {
+    JsonObject& data = (*root)["d"];
+    JsonObject& info = (*root)["info"];
+    data["myName"] = DEVICE_NAME;
+    static float t_dht = 0;
+    static float h_dht = 0;
+
+
+    data["temp"] = t_dht;
+    data["humid"] = h_dht;
+
+    info["author"] = AUTHOR;
+    info["board"]  = BOARD;
+    info["sensor"] = SENSOR;
+};
---
+++ examples/basic_mqtt/basic_mqtt.ino
@@ -0,0 +1,59 @@
+#include <MqttConnector.h>
+#include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+
+MqttConnector *mqtt;
+
+
+#define MQTT_PORT        1883
+#define PUBLISH_EVERY    3*1000 // every 15 seconds
+
+/* comment ทิ้งถ้าไม่ส่ username/password */
+
+
+#define MQTT_HOST     "mqtt.espert.io"
+// #define MQTT_USERNAME "z7KXagr95sny6Zx"
+// #define MQTT_PASSWORD "jPxRZFXfoshDxLHk79ChVFLu5/8="
+#define MQTT_CLIENT_ID "OHXceTmPuUaYt41A"
+#define MQTT_PREFIX ""
+
+/* SENSOR INFO */
+#define DEVICE_NAME "DEEPSLEEP-001"
+#define AUTHOR      "Nat Weerawan"
+#define BOARD       "NODEMCU V1.0"
+#define SENSOR      "DHT22"
+
+/* WIFI INFO */
+#define WIFI_SSID        ""
+#define WIFI_PASSPHARSE  ""
+
+#include "_publish.h"
+#include "_receive.h"
+#include "init_mqtt.h"
+
+
+
+void init_hardware()
+{
+
+  Serial.begin(115200);
+  delay(10);
+  Serial.println();
+  Serial.println("BEGIN");
+}
+
+void setup()
+{
+  WiFi.begin("Nat", "123456789");
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.println("CONNECTING...");
+    delay(300);
+  }
+  init_hardware();
+  init_mqtt();
+}
+
+void loop()
+{
+  mqtt->loop();
+}
