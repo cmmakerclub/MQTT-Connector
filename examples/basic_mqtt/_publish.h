eb618ef6be76b8190f9a300f4b6161f1099a8c37
Nat Weerawan <nat.wrw@gmail.com>
2016-05-25 11:17:41 +0700

    Update basic example.

--- examples/basic_mqtt/_publish.h
+++ examples/basic_mqtt/_publish.h
@@ -1,3 +1,5 @@
+#include <MqttConnector.h>
+
 MqttConnector::prepare_data_hook_t on_prepare_data =
 [&](JsonObject * root) -> void {
     float t_dht = 0;
@@ -7,5 +9,5 @@ MqttConnector::prepare_data_hook_t on_prepare_data =
     JsonObject& info = (*root)["info"];
 
     data["myName"] = DEVICE_NAME;
-    data["millis"] = millis();
+    // data["millis"] = millis();
 };
