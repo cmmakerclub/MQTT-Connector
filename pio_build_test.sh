b5c0312acccd16d8403967d0176b465d47a2e5f3
Nat <nat@cmmc.io>
2018-01-29 15:41:44 +0700

    add build script.

---
+++ pio_build_test.sh
@@ -0,0 +1,4 @@
+
+platformio ci --lib="." --board=espresso_lite_v2 "examples/basic_bme280"
+platformio ci --lib="." --board=espresso_lite_v2 "examples/basic_mqtt"
+#platformio ci --lib="." --board=espresso_lite_v2 "examples/basic_dht"
