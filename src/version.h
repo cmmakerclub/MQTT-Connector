b6dbdaa1390eee44d20d1a71284f8758a3cf5110
Nat <nat@cmmc.io>
2018-01-30 08:56:10 +0700

    add version code.

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 /* Generated file, do not edit. */
-#define APP_VERSION "v1.0.0b"
-#define APP_VERSION_DATE "2018-01-29 15:52:03 +0700"
+#define APP_VERSION "v1.0.1"
+#define APP_VERSION_DATE "2018-01-30 08:55:17 +0700"
