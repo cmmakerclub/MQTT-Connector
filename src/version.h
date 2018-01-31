9f12ba3c4f5eeee80bb5a600b704876654e24120
Nat <nat@cmmc.io>
2018-01-31 15:35:46 +0700

    update v1.0.3

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 /* Generated file, do not edit. */
-#define APP_VERSION "v1.0.2"
-#define APP_VERSION_DATE "2018-01-30 18:05:56 +0700"
+#define APP_VERSION "v1.0.3"
+#define APP_VERSION_DATE "2018-01-31 15:34:48 +0700"
