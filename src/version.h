ea83635f61b7ca55730558cd968c438580d7828c
Nat <nat@cmmc.io>
2018-05-09 15:57:21 +0700

    Update library version for V1.0.5

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
 /* Generated file, do not edit. */
-#define APP_VERSION "v1.0.4+build.4.ga16b133"
-#define APP_VERSION_DATE "2018-05-09 15:55:15 +0700"
+#define APP_VERSION "1.0.5"
+#define APP_VERSION_DATE "2018-05-09 15:57:21 +0700"
