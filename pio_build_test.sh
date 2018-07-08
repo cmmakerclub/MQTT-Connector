fbb5600d074eca18e4375e9e5f1f1ec11dd3625e
Nat <nat@cmmc.io>
2018-07-08 11:09:16 +0700

    update pio loop.

--- pio_build_test.sh
+++ pio_build_test.sh
@@ -8,8 +8,4 @@ rm -r test
 for EXAMPLE in $PWD/examples/*; 
 do
 	platformio ci --project-option="lib_ldf_mode=2" $EXAMPLE -l '.' -b $BOARD
-done
-
-# platformio ci --project-option="lib_ldf_mode=2" --lib="." --board=espresso_lite_v2 "examples/basic_bme280"
-# platformio ci --project-option="lib_ldf_mode=2" --lib="." --board=espresso_lite_v2 "examples/basic_mqtt"
-# platformio ci --project-option="lib_ldf_mode=2" --lib="." --board=espresso_lite_v2 "examples/basic_dht"
+done 
